97c604eed8624be15ac5130292495ad286efcf08
Paolo Posso <pvictorsys@gmail.com>
2021-02-15 18:52:47 -0300

    test travis

--- test/service_test.go
+++ test/service_test.go
@@ -31,3 +31,27 @@ func Test_ShouldGenerateUserId(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func Test_ShouldBuild(t *testing.T) {
+	repo, err := memory.NewMemoryRepository()
+
+	if err != nil {
+		fmt.Println(err)
+		panic("MemoryRepository could not be injected")
+	}
+	
+	service := user.NewUserService(repo)
+	
+	user := user.User{}
+	user.Name = "Paolo"
+	user.Password = "123456"
+	user.Email = "paolo@paolo.com"
+
+	service.Register(&user)
+
+	fmt.Println(user.Id)
+
+	if user.Id == "" {
+		t.Fail()
+	}
+}
